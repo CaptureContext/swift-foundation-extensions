443e2862c530ce7428ac2bb7d2f9458302930ee0
Maxim Krouk <id.maximkrouk@gmail.com>
2023-08-06 02:56:17 +0200

    feat: Add PartialRange.clamped and ClosedRange<Numeric>.length methods

--- Sources/FoundationExtensions/Extensions/Range+.swift
+++ Sources/FoundationExtensions/Extensions/Range+.swift
@@ -1,9 +1,9 @@
-extension ClosedRange where Bound: FloatingPoint {
+extension Range where Bound: BinaryInteger {
   @inlinable
   public var length: Bound { upperBound - lowerBound }
 }
 
-extension ClosedRange where Bound: BinaryInteger {
+extension ClosedRange where Bound: Numeric {
   @inlinable
   public var length: Bound { upperBound - lowerBound }
 }
@@ -14,6 +14,28 @@ extension ClosedRange {
     if value < lowerBound {
       return lowerBound
     } else if value > upperBound {
+      return upperBound
+    } else {
+      return value
+    }
+  }
+}
+
+extension PartialRangeFrom {
+  @inlinable
+  public func clamped(_ value: Bound) -> Bound {
+    if value < lowerBound {
+      return lowerBound
+    } else {
+      return value
+    }
+  }
+}
+
+extension PartialRangeThrough {
+  @inlinable
+  public func clamped(_ value: Bound) -> Bound {
+    if value > upperBound {
       return upperBound
     } else {
       return value
