b107441ec9df3caf4bd195fae770c4cfd820395c
maximkrouk <id.maximkrouk@gmail.com>
2026-01-09 16:41:47 +0100

    feat: Support Swift 6.1

---
+++ Package@swift-6.1.swift
@@ -0,0 +1,73 @@
+// swift-tools-version: 6.1
+
+import PackageDescription
+
+let package = Package(
+	name: "swift-foundation-extensions",
+	platforms: [
+		.macOS(.v10_15),
+		.macCatalyst(.v13),
+		.iOS(.v13),
+		.tvOS(.v13),
+		.watchOS(.v6)
+	],
+	products: [
+		.library(
+			name: "FoundationExtensions",
+			targets: ["FoundationExtensions"]
+		),
+		.library(
+			name: "FoundationExtensionsMacros",
+			targets: ["FoundationExtensionsMacros"]
+		),
+	],
+	dependencies: [
+		.package(
+			url: "https://github.com/capturecontext/swift-resettable.git",
+			.upToNextMinor(from: "0.1.0")
+		),
+		.package(
+			url: "https://github.com/capturecontext/swift-equated.git",
+			.upToNextMinor(from: "0.0.1")
+		),
+		.package(
+			url: "https://github.com/capturecontext/swift-associated-objects.git",
+			.upToNextMinor(from: "0.1.0")
+		)
+	],
+	targets: [
+		.target(
+			name: "FoundationExtensions",
+			dependencies: [
+				.product(
+					name: "AssociatedObjects",
+					package: "swift-associated-objects"
+				),
+				.product(
+					name: "Resettable",
+					package: "swift-resettable"
+				),
+				.product(
+					name: "Equated",
+					package: "swift-equated"
+				),
+			]
+		),
+		.target(
+			name: "FoundationExtensionsMacros",
+			dependencies: [
+				.target(name: "FoundationExtensions"),
+				.product(
+					name: "AssociatedObjectsMacros",
+					package: "swift-associated-objects"
+				),
+			]
+		),
+		.testTarget(
+			name: "FoundationExtensionsTests",
+			dependencies: [
+				.target(name: "FoundationExtensions"),
+			]
+		),
+	]
+)
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # swift-foundation-extensions
 
-[![CI](https://github.com/CaptureContext/swift-foundation-extensions/actions/workflows/ci.yml/badge.svg)](https://github.com/CaptureContext/swift-foundation-extensions/actions/workflows/ci.yml) [![SwiftPM 6.2](https://img.shields.io/badge/Swift-6.2-ED523F.svg?style=flat)](https://swift.org/download/) ![Platforms](https://img.shields.io/badge/Platforms-iOS_13_|_macOS_10.15_|_tvOS_14_|_watchOS_7-ED523F.svg?style=flat) [![@capture_context](https://img.shields.io/badge/contact-@capturecontext-1DA1F2.svg?style=flat&logo=twitter)](https://twitter.com/capture_context) 
+[![CI](https://github.com/CaptureContext/swift-foundation-extensions/actions/workflows/ci.yml/badge.svg)](https://github.com/CaptureContext/swift-foundation-extensions/actions/workflows/ci.yml) [![SwiftPM 6.2](https://img.shields.io/badge/Swift-6.2_|_6.1-ED523F.svg?style=flat)](https://swift.org/download/) ![Platforms](https://img.shields.io/badge/Platforms-iOS_13_|_macOS_10.15_|_tvOS_14_|_watchOS_7-ED523F.svg?style=flat) [![@capture_context](https://img.shields.io/badge/contact-@capturecontext-1DA1F2.svg?style=flat&logo=twitter)](https://twitter.com/capture_context) 
 
 Standard extensions for Foundation framework
 
