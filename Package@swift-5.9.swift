7ca40700fc91954da50f5a3c4c4e3b75b35cf914
maximkrouk <id.maximkrouk@gmail.com>
2026-02-05 06:08:24 +0100

    feat: Update dependencies

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -24,7 +24,7 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/capturecontext/swift-resettable.git",
-			.upToNextMinor(from: "0.1.0")
+			.upToNextMinor(from: "0.2.0")
 		),
 		.package(
 			url: "https://github.com/capturecontext/swift-equated.git",
