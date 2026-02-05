eecd7217028bb438c0fe155277b9a5b06096a6af
maximkrouk <id.maximkrouk@gmail.com>
2026-02-05 17:14:27 +0100

    fix: Swift 5.9 compatibility

--- Sources/FoundationExtensions/Extensions/DispatchTime+.swift
+++ Sources/FoundationExtensions/Extensions/DispatchTime+.swift
@@ -8,13 +8,19 @@ extension DispatchTimeInterval {
 	}
 }
 
-extension DispatchTime: @retroactive ExpressibleByFloatLiteral {
+#if swift(>=6.0)
+extension DispatchTime: @retroactive ExpressibleByFloatLiteral {}
+#else
+extension DispatchTime: ExpressibleByFloatLiteral {}
+#endif
+
+extension DispatchTime {
 	/// Creates DispatchTime for the specified interval in seconds from `.now()`
 	@inlinable
 	public static func interval(_ interval: TimeInterval) -> DispatchTime {
 		return .now() + .interval(interval)
 	}
-	
+
 	@inlinable
 	public init(floatLiteral value: TimeInterval) {
 		self = .interval(value)
