b4affe3dee6e543fedde24d445e8c92c93bd64c7
maximkrouk <id.maximkrouk@gmail.com>
2026-03-10 07:23:54 +0100

    feat: Update KeyPathsExtensions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 
 import PackageDescription
 
@@ -24,7 +24,7 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/capturecontext/swift-resettable.git",
-			.upToNextMinor(from: "0.2.0")
+			.upToNextMinor(from: "0.3.0")
 		),
 		.package(
 			url: "https://github.com/capturecontext/swift-equated.git",
