0f1cc4852f8ca8ef324fe7ad024246ab1ef87674
maximkrouk <id.maximkrouk@gmail.com>
2026-09-03 14:39:39 +0200

    chore: update Resettable dependency

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/capturecontext/swift-resettable.git",
-			.upToNextMinor(from: "0.3.2")
+			.upToNextMinor(from: "0.3.3")
 		),
 		.package(
 			url: "https://github.com/capturecontext/swift-equated.git",
